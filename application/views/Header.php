758cc041ada3b1348343d7cd813d6ea46480a158
Hugo <hugohay00@outlook.com>
2021-03-23 14:17:18 +0000

    more files added

---
+++ application/views/Header.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel='stylesheet' href="<?php echo base_url('public\css\bootstrap.css') ?>"/>
+	<!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+    <script src="<?php echo base_url('public\javascript\bootstrap.js') ?>"></script>
+	<title>Settlers of Catan</title>
+</head>
---
+++ application/views/template.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel='stylesheet' href="<?php echo base_url('public\css\bootstrap.css') ?>"/>
+	<!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+    <script src="<?php echo base_url('public\javascript\bootstrap.js') ?>"></script>
+	<title>Template</title>
+</head>
+
+<body>
+
+<!-- Template created 23/03/2021 -->
+<!-- Change the ID to the view name and enter content within the div according to your design -->
+   
+    <div class="container .bg-info.bg-gradient" id="ENTER VIEW NAME">
+        <!-- enter content here -->
+        <!-- will be pushed into the main project once done-->
+
+    </div>
+</body>
+	
+</html>
