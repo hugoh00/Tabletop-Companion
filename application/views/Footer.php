e82724af828f54ea2b6be676b5065232a2407c27
Hugo <hugohay00@outlook.com>
2021-03-25 21:28:25 +0000

    dynamic added increase decrease Uncaught ReferenceError: brick is not defined     at HTMLButtonElement.onclick

--- application/views/Footer.php
+++ application/views/Footer.php
@@ -8,10 +8,14 @@ $(function(){
 //new column increase decrease 
 // dynamic id multiple ifs to check which to increase/decrease
 
-function increaseDecrease(i) 
+function increaseDecrease(resource, i) 
 {
+    //plusminusResourcename
     if (i == 1) {
-
+        if (resource=='brick') {
+            var resource = $('#brick1').val();
+            $('#brick1').val() = resource + 1;
+        }
     }
 }
 function startGame() 
@@ -24,25 +28,30 @@ function startGame()
         $('#gameSetup').hide();
         $('#gameActive').show();
 
-        var up = '<button class="btn btn-success" onclick="increaseDecrease()"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M3.22 9.78a.75.75 0 010-1.06l4.25-4.25a.75.75 0 011.06 0l4.25 4.25a.75.75 0 01-1.06 1.06L8 6.06 4.28 9.78a.75.75 0 01-1.06 0z"></path></svg></button>';
-        var down = '<button class="btn btn-danger" onclick="increaseDecrease()"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M12.78 6.22a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06 0L3.22 7.28a.75.75 0 011.06-1.06L8 9.94l3.72-3.72a.75.75 0 011.06 0z"></path></svg></button>';
-   
-
         for (var i=1; i <= $('#playerSelect').val(); i++) 
         {
             //append rows
-            //svg
-            var up = '<button class="btn btn-success" onclick="increaseDecrease(' + i + ')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M3.22 9.78a.75.75 0 010-1.06l4.25-4.25a.75.75 0 011.06 0l4.25 4.25a.75.75 0 01-1.06 1.06L8 6.06 4.28 9.78a.75.75 0 01-1.06 0z"></path></svg></button>';
-            var down = '<button class="btn btn-danger" onclick="increaseDecrease(' + i + ')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M12.78 6.22a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06 0L3.22 7.28a.75.75 0 011.06-1.06L8 9.94l3.72-3.72a.75.75 0 011.06 0z"></path></svg></button>';
-            //dynamic id setting
+             //dynamic id setting
             var brick = "brick" + i;
             var ore = "ore" + i;
             var lumber = "lumber" + i;
             var wool = "wool" + i;
-            var row = "<tr><td>"+ i + "</td> <td>5</td><td>" + up + down + "</td> <td>5</td><td>" + up + down + "</td><td>5</td><td>" + up + down + "</td><td>5</td><td>" + up + down + "</td></tr>";
+            var row = "<tr><td>"+ i + "</td> <td id=" + brick + ">5</td><td id='plusminusBrick'>" + up('brick', i) + down('brick', i) + "</td> <td id=" + ore + ">5</td><td id='plusminusOre'>" +  up('ore', i) + down('ore', i) + "</td><td id=" + lumber + ">5</td><td id='plusminusLumber'>" + up('brick', i) + down('brick', i) + "</td><td id=" + wool + ">5</td><td id='plusminusWool'>" +  up('brick', i) + down('brick', i) + "</td></tr>";
             $('#populate').append(row);
         }
     }
+    
+}
+
+function up(resource, i) {
+    var RESOURCE = resource.toString();
+    return '<button class="btn btn-success" onclick="increaseDecrease(' + RESOURCE + ',' + i + ')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M3.22 9.78a.75.75 0 010-1.06l4.25-4.25a.75.75 0 011.06 0l4.25 4.25a.75.75 0 01-1.06 1.06L8 6.06 4.28 9.78a.75.75 0 01-1.06 0z"></path></svg></button>';
+
+}
+
+function down(resource, i) {
+    var RESOURCE = resource.toString();
+    return '<button class="btn btn-danger" onclick="increaseDecrease(' + RESOURCE + ',' + i + ')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M12.78 6.22a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06 0L3.22 7.28a.75.75 0 011.06-1.06L8 9.94l3.72-3.72a.75.75 0 011.06 0z"></path></svg></button>';
     
 }
 
