50e1f1b0d78b388d6a1592b1692580d18e65c517
Hugo <hugohay00@outlook.com>
2021-03-29 15:07:10 +0100

    added victory points card

--- application/views/gamehosting.php
+++ application/views/gamehosting.php
@@ -39,6 +39,7 @@
                 </div>
                 <button type="button" onclick="rollDice()" class="btn btn-dark btn-lg">Roll Dice</button>
             </div>
+            </br>
             <!-- storing the svgs for later use -->
 
                 <!-- Player SVG -->
@@ -162,6 +163,29 @@
                     </div>
                 </div>
             </div>
+            <!-- victory points -->
+              <!-- recipes for construction -->
+              <div class="card text-white bg-dark mb-3" style="max-width: 100%;">
+                <div class="card-header">Victory Points</div>
+                <div class="card-body">
+                    <div class="table-responsive-md">
+                        <table class="table table-dark">
+                            <thead class="thead-dark">
+                                <tr class="table-info">
+                                    <th scope="col"><?php echo $player;?>
+                                    </th>
+                                    <!-- Victory Points -->
+                                    <th scope="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gem" viewBox="0 0 16 16">
+                                    <path d="M3.1.7a.5.5 0 0 1 .4-.2h9a.5.5 0 0 1 .4.2l2.976 3.974c.149.185.156.45.01.644L8.4 15.3a.5.5 0 0 1-.8 0L.1 5.3a.5.5 0 0 1 0-.6l3-4zm11.386 3.785l-1.806-2.41-.776 2.413 2.582-.003zm-3.633.004l.961-2.989H4.186l.963 2.995 5.704-.006zM5.47 5.495L8 13.366l2.532-7.876-5.062.005zm-1.371-.999l-.78-2.422-1.818 2.425 2.598-.003zM1.499 5.5l5.113 6.817-2.192-6.82L1.5 5.5zm7.889 6.817l5.123-6.83-2.928.002-2.195 6.828z"/>
+                                </svg></th>
+                                </tr>
+                            </thead>
+                            <tbody id="victory">
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+              </div>
             <!-- button to close game  -->
             <div class="col text-center">
                         <button type="button" onclick="endGame()" class="btn btn-block btn-dark">End Game</button>
