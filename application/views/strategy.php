7f4b6b8662d50b7b124c1e696fd4af0af2fee3d6
BenjaminBee <45979666+BenjaminBee@users.noreply.github.com>
2021-03-30 14:11:54 +0100

    Add files via upload
    creating basic layout

--- application/views/strategy.php
+++ application/views/strategy.php
@@ -1,3 +1,78 @@
-<div class="container bg-gradient-danger" id="strategy" style="background-color: #f4d032;">
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel='stylesheet' href="<?php echo base_url('public\css\bootstrap.css') ?>"/>
+	<!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+    <script src="<?php echo base_url('public\javascript\bootstrap.js') ?>"></script>
+	<title>Strategy</title>
+</head>
 
-</div>
+<body>
+
+<!-- Template created 23/03/2021 -->
+<!-- Change the ID to the view name and enter content within the div according to your design -->
+   
+<br>
+<body style="background-color: #be5830;">
+
+<!-- Template created 23/03/2021 -->
+<!-- Change the ID to the view name and enter content within the div according to your design -->
+   
+    <div class="container bg-gradient-danger" id="strategy" style="background-color: #f4d032;">
+        <!-- enter content here -->
+        <!-- will be pushed into the main project once done-->
+        <h3 class="text-center">Strategy</h3>
+        <div class="container">
+            <div class="row">
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    <h4 class="text-center">Strategy</h4>
+                </div>
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    <h4 class="text-center">Strategy</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    This rounded container has a background color.
+                </div>
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    This rounded container has a background color.
+                </div>
+            </div>
+            <div class="row">
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    This rounded container has a background color.
+                </div>
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    This rounded container has a background color.
+                </div>
+            </div>
+            <div class="row">
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    This rounded container has a background color.
+                </div>
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    This rounded container has a background color.
+                </div>
+            </div>
+            <div class="row">
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    This rounded container has a background color.
+                </div>
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    This rounded container has a background color.
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+	
+</html>
