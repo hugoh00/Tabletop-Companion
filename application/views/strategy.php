0b1340a96c3c93d789d17351eee4eac5d8fd39e5
BenjaminBee <45979666+BenjaminBee@users.noreply.github.com>
2021-04-20 15:20:23 +0100

    Add files via upload
    Added content

--- application/views/strategy.php
+++ application/views/strategy.php
@@ -5,42 +5,155 @@
         <div class="container">
             <div class="row">
                 <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    <h4 class="text-center">Strategy</h4>
+                    <h4 class="text-center">Longest Road</h4>
+                    <p>
+                    This relies on large collections of wood & brick to complete a number of objectives
+                    </p>
+                    <li>Build lots of settlements</li>
+                    <li>Building future settlements on grain/ore hexes in order to build more cities</li>
+                    <li>Build roads strategically to cut off other players expansions to maximise your ability to get grain/ore for building more cities</li>
+                    <p>
+                    Overall this a strategy that requires careful planning and placement in order to maximise resource gathering.
+                    </p>
                 </div>
                 <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    <h4 class="text-center">Strategy</h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    This rounded container has a background color.
-                </div>
-                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    This rounded container has a background color.
-                </div>
-            </div>
-            <div class="row">
-                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    This rounded container has a background color.
-                </div>
-                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    This rounded container has a background color.
-                </div>
-            </div>
-            <div class="row">
-                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    This rounded container has a background color.
-                </div>
-                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    This rounded container has a background color.
+                    <h4 class="text-center">Largest Army</h4>
+                    <p>
+                    This too relies on gathering lots of grain/ore. 
+                    Overall this is a solid strategy to allow the player to guarantee cities at the beginning. As long as you have development cards to protect from the robber.
+                    </p>
+                    <br>
+                    <p>
+                    This allows you then to either expand towards wood/brick by trading with your grain/ore with other players.
+                    </p>
                 </div>
             </div>
             <div class="row">
                 <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    This rounded container has a background color.
+                    <h4 class="text-center">Initial Settlement Placement</h4>
+                    <p>
+                        Try to get your initial settlements to a total dot count of 11-13. 
+                        This generally leads to strong resource production.
+                    </p>
+                    <br>
+                    <p>You'll want to:</p>
+                    <li>Always put the first 2 settlements on 3 resource hex possibilities</li>
+                    <li>Spread your settlements on as many different numbers as possible so this doesn't lead to robber losses later on</li>
+                    <li>Take your time, review the board and try to put a settlement on the rarest resource you see</li>
                 </div>
                 <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
-                    This rounded container has a background color.
+                    <h4 class="text-center">Expansion Opportunity Priorities</h4>
+                    <li>Go for the spot with the most dots</li>
+                    <li>Increase diversity of resources you produce</li>
+                    <li>Try to get to a nearby port</li>
+                    <br>
+                    <li>Make sure you aren't cut off from future expansion opportunies. Utilise your roads!</li>
+                    <li>Don't place settlements to mess up plans of other players. Think about what most benefits you first</li>
+                    <li>Try to put your settlements/cities on hexes that have other players on them so that the hex is shared. Keeping the robber off your hexes since other players are involved in protecting the hex as well!</li>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    <h4 class="text-center">Expansion/settlements</h4>
+                    <p>
+                    Build settlements early, growth is exponential once you start up. Build settlements and cities first to generate more production later on
+                    </p>
+                    <br>
+                    <p>
+                    Break the longest road if at all possible with a settlement and attempt to get settlements on 3 different types of resources. This means more trading options later on. However, if you can't, put your settlements on:
+                    </p>
+                    <br>
+                    <li>Wood/brick - settlements</li>
+                    <li>Grain/ore - cities</li>
+                    <li>Sheep. However, generally they are always in high supply so don't worry too much about getting them</li>
+                </div>
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    <h4 class="text-center">Robber/Knight Tips</h4>
+                    <p>
+                    If the robber is on your settlement/city before you roll the dice and you have a knight card, 
+                    play the knight card before you roll so that if you roll the number that the 
+                    robber was on before, you will get that resource.
+                    </p>
+                    <br>
+                    <p>However, don't worry about buying too many development cards early on. It constricts your growth rate of resource production compared to building roads/settlements/cities
+                    </p>
+                    <br>
+                    <p>
+                    Make sure you always have less than 7 cards before someone rolls the dice, even if it hurts your trading chances later
+                    , in case a 7 is rolled. Additionaly, make sure you buy a development card or another if necessary to avoid having more than 7 cards in your hand before the dice are rolled
+                    </p>
+                    <br>
+                    <p>
+                    If a 7 is rolled, put the robber on the rare resource hex, cities if possible and preferably target the current game leader by placing the robber on their rarest resource hex that has a city on it
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    <h4 class="text-center">Trading</h4>
+                    <p>
+                    Helpful tips!
+                    </p>
+                    <li>Don't trade with someone when they are about to win.</li>
+                    <li>Trade with people who are behind you or slightly even with you</li>
+                    <li>During the early to middle phase of the game, its generally safe to trade with anyone</li>
+                    <li>Try to be a part of every trade possible as long as it helps you as much as your opponent. Be tactical!</li>
+                </div>
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    <h4 class="text-center">Ports</h4>
+                    <p>
+                    Ports are helpful, they (2:1) can be especially useful if you can get another settlement on that resource type with a number of 6 or 8 for example.
+                    </p>
+                    <br>
+                    <p>
+                    3:1 ports may be more useful than 2:1 ports though because of the lack of risk of the robber affecting your port trading ability. Since 3:1 allows you to trade any resource compared to a 2:1 that takes a specific resource
+                    </p>
+                    <br>
+                    <li>Placing a settlement near a port at the beginning of the game but not directly on the port can be a good idea in terms of future accessiblity</li>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    <h4 class="text-center">Development Cards</h4>
+                    <p>Development cards are as follows depending on the deck used:</p>
+                    <li>Soldier - 48%</li>
+                    <li>Monopoly - 12%</li>
+                    <li>Road building - 12%</li>
+                    <li>Year of plenty - 12%</li>
+                    <li>Victory point - 16%</li>
+                    <br>
+                    <p>
+                    Therefore, unless you are going for the largest army or just wanted to buy a development card to avoid having more than 7 cards in your hand (due to robber)
+                    It is likely a better investment to build roads/settlements/cities since they guarantee victory points compared to the unknown that is development cards.
+                    </p>
+                    <br>
+                    <p>
+                    If you are going for the grain/ore strategy, its recommended to invest in at least 1 development card to try and get a knight to help keep the robber off your city!
+                    </p>
+                    <br>
+                    <p>
+                    Generally, development cards are only a good investment when:
+                    </p>
+                    <li>You have more than 7 cards in your hand but have no other options to build anything else</li>
+                    <li>If you are trying to win with the grain/ore (city) strategy and need knights to keep the robber off your hexes</li>
+                    <li>If a lot of development cards have been bought already (and they were soldiers), so there is a better chance the remaining are non-soldier cards</li>
+                </div>
+                <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                    <h4 class="text-center">Monopoly Card Trick</h4>
+                    <p>
+                        This is incredibly hard to pull off but very rewarding.
+                        Essentially, you trade away all of one resource to the rest of the players
+                        in exchange for whatever they have.
+                    </p>
+                    <br>
+                    <p>
+                        The next part then involves the monopoly card to get it all back
+                        Then, you have all the resources that you traded for, in addition to the resource that you just traded to the rest of the players.
+                    </p>
+                    <br>
+                    <p>
+                        Be warned, this can easily lead to a trade embargo for a game and very suspicious trading partners!
+                    </p>
                 </div>
             </div>
         </div>
