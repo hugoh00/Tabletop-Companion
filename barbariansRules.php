c513b93b9772e1dcf69e85dae21416556135cde8
BenjaminBee <45979666+BenjaminBee@users.noreply.github.com>
2021-04-27 14:51:47 +0100

    Add files via upload
    Created barbarians view

---
+++ barbariansRules.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Template created 23/03/2021 -->
+<!-- Change the ID to the view name and enter content within the div according to your design -->
+   
+<div class="container bg-gradient-danger" id="rules" style="background-color: #f4d032;">
+    <!-- Traders & Barbarians -->
+    <div id="traderbarbarians34">
+    <h3 class="text-center">Traders & Barbarians</h3>
+    <div class="container">
+        <div class="row">
+            <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                <h4 class="text-center">Introduction</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                <h4 class="text-center">Game Components</h4>
+            </div>
+            <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                <h4 class="text-center">Game Rules</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                <h4 class="text-center">Barbarian Attack!</h4>
+            </div>
+            <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                <h4 class="text-center">Additional Rules</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                <h4 class="text-center">3-4 Vs 5-6 Players</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container rounded col-sm px-2 m-2" style="border:1px solid black">
+                <h4 class="text-center">Special Rules</h4>
+            </div>
+        </div>
+    </div>
+
+</div>
+	
+</html>
